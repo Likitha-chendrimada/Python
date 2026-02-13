545c454da546102d9b79a174332b2594d497b179
Likitha <likithasuguna2006@gmail.com>
2026-02-13 19:51:40 +0530

    GFG-P1
    Implement numberMatcher function to extract numbers from a string using regular expressions.

--- regex.py
+++ regex.py
@@ -1,3 +1,10 @@
+##Write a Python function numberMatcher(str) that uses Regular Expressions to extract and print all numbers present in the given string str.
+##If numbers are found, print them separated by space.
+##If no numbers are found, print -1.
+
+
+
+
 import re
 def numberMatcher(s):
     pat=r'\d+'   # matches one or more digits
