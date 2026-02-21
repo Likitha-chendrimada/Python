8ce362c5c718b67dff161b9d0312d2a882866624
Likitha <likithasuguna2006@gmail.com>
2026-02-21 17:05:40 +0530

    Fix comment formatting in index.p

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-#Given a tuple arr with distinct elements and an integer x, find the index position of x. Assume to have x in the tuple always. Print the index (0-based).
+ #Given a tuple arr with distinct elements and an integer x, find the index position of x. Assume to have x in the tuple always. Print the index (0-based).
 
 arr=tuple(map(int, input().split()))
 x=int(input())
