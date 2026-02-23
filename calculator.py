28c4473f932398b494479be1bd992cbc093c114c
Likitha <likithasuguna2006@gmail.com>
2026-02-23 19:01:38 +0530

    GFG-P14

--- calculator.py
+++ calculator.py
@@ -4,6 +4,7 @@ If operator equals to 2 subtract b from a, then print the result as a string.
 If operator equals to 3 multiply a and b, then print the result as a string.
 If operator equals to any another number, print "Invalid Input"(without quotes)."""
 
+
 def utility(a, b, opr):
     if opr==1:
         print(str(a+b),end="")
