49032784a96d4db70998791bf5acc9399fc99404
Likitha <likithasuguna2006@gmail.com>
2026-02-24 22:44:07 +0530

    GFG-P19

---
+++ set.py
@@ -0,0 +1,11 @@
+# Function to insert
+def insert(s,element):
+    s.add(element)
+
+# Function to remove element from set
+def remove_from_set(s,element):
+    s.discard(element)  
+
+# Function to find sum of elements in set
+def sum_set(s):
+    return sum(s)
