7d0b4b05fdf68f4efe9aec5ef456457a75ad6fa7
Likitha <likithasuguna2006@gmail.com>
2026-02-24 22:49:34 +0530

    GFG-P20

---
+++ set2.py
@@ -0,0 +1,11 @@
+# Function to find common elements in sets
+def common_in_set(a, b):
+    return a&b
+
+# Function to find difference in sets
+def diff(a,b):
+    return a-b 
+
+# Function to find union of sets
+def all_in_set(a, b):
+    return a|b
