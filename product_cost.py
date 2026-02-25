a8403fa1e2299a28c52a3a2b68ff953736ff702f
Likitha <likithasuguna2006@gmail.com>
2026-02-25 23:48:07 +0530

    GFG-P23

--- product_cost.py
+++ product_cost.py
@@ -1,5 +1,6 @@
 def filter_high_rated_expensive(df):
     # Filter products that satisfy all conditions
+    
     filtered_df=df[ (df['rating']>=4.5) & (df['quantity_in_stock']>0)& (df['price']>=300) ]
     # Select required columns
     result=filtered_df[ ['product_id','product_name','rating','quantity_in_stock','price']]
